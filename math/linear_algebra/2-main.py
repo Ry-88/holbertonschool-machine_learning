96fe178314c927b07e8cc7189bda36facc79f384
Rayan Hantoul <ryan-055@outlook.com>
2026-02-21 19:12:20 +0300

    change file

--- math/linear_algebra/2-main.py
+++ math/linear_algebra/2-main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
+cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
 
 mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
+mat2 = [[5, 6]]
+mat3 = [[7], [8]]
+mat4 = cat_matrices2D(mat1, mat2)
+mat5 = cat_matrices2D(mat1, mat3, axis=1)
+print(mat4)
+print(mat5)
+mat1[0] = [9, 10]
+mat1[1].append(5)
+print(mat1)
+print(mat4)
+print(mat5)
