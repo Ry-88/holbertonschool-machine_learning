23b2a7baa7fd54ae82abf8456eae73e2e764da54
Rayan Hantoul <ryan-055@outlook.com>
2026-02-21 21:28:19 +0300

    correct code

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-"""Write a function that calculates
-the shape of a numpy.ndarray"""
+"""Function that calculates the shape of a numpy.ndarray"""
 
 
 def np_shape(matrix):
-    """Calculates the shape of a numpy.ndarray"""
-    # Check if it has shape attribute (duck typing for numpy arrays)
-    if not hasattr(matrix, 'shape'):
+    """Return the shape of a numpy.ndarray"""
+    if not hasattr(matrix, "shape"):
         return None
     return list(matrix.shape)
