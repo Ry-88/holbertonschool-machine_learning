d97e6676b6457b7936d33f0749d81cca8dcecbb7
Rayan Hantoul <ryan-055@outlook.com>
2026-03-09 01:38:53 +0300

    Calculates the value of the PDF

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -38,3 +38,12 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value of a given z-score."""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value."""
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
+
+        return (1 / (self.stddev * (2 * pi) ** 0.5)) * (e ** exponent)
