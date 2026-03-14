39742e5bb740e30668c93a1815839cc9eaf93f47
Rayan Hantoul <ryan-055@outlook.com>
2026-03-14 23:02:10 +0300

    fix code

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -6,7 +6,7 @@ Module that slices specific columns and rows from a DataFrame.
 
 def slice(df):
     """
-    Extract the columns High, Low, Close, and Volume_BTC,
+    Extract the columns High, Low, Close, and Volume_(BTC),
     then select every 60th row.
 
     Args:
@@ -15,4 +15,4 @@ def slice(df):
     Returns:
         pd.DataFrame: The sliced DataFrame.
     """
-    return df[["High", "Low", "Close", "Volume_BTC"]].iloc[::60]
+    return df[["High", "Low", "Close", "Volume_(BTC)"]].iloc[::60]
