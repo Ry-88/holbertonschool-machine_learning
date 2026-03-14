e8281280dc24a00208e91ebb8ec196e18e0882e2
Rayan Hantoul <ryan-055@outlook.com>
2026-03-14 23:41:32 +0300

    fix code

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,12 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-    y = np.arange(0, 11) ** 3
-    x = np.arange(0, 11)  # x من 0 إلى 10
+    x = np.arange(0, 11)        # x من 0 إلى 10
+    y = x ** 3                   # y مكعب x
     plt.figure(figsize=(6.4, 4.8))
 
-    # رسم y مقابل x كخط أحمر متصل
-    plt.plot(x, y, color='red', linestyle='solid')
+    # رسم الخط الأحمر المكعب
+    plt.plot(x, y, 'r', linestyle='-')
+
+    # ضبط حدود x-axis من 0 إلى 10
+    plt.xlim(0, 10)
 
     # عرض الرسم
     plt.show()
